11d30e530aeb2324c782134133f5c4a9b21467a1
sanya-shch <shchipkov.sanya@ukr.net>
2021-03-28 20:40:02 +0300

    add simple dnd;

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+
+const defaultData = [
+  { title: "group 1", items: ["0", "1", "2", "3"] },
+  { title: "group 2", items: ["4", "5", "6"] },
+  { title: "group 3", items: ["7", "8"] },
+  { title: "group 3", items: ["9"] },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="drag-n-drop">
+        {defaultData.map((grp, grpI) => (
+          <div key={grp.title} className="dnd-group">
+            {grp.items.map((item, itemI) => (
+              <div draggable key={item} className="dnd-item">
+                {item}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      {/* <div className="drag-n-drop">
+        <div className="dnd-group">
+          <div className="group-title">Group 1</div>
+          <div className="dnd-item">
+            <div>
+              <p>ITEM 1</p>
+            </div>
+          </div>
+          <div className="dnd-item">
+            <div>
+              <p>ITEM 2</p>
+            </div>
+          </div>
+          <div className="dnd-item">
+            <div>
+              <p>ITEM 3</p>
+            </div>
+          </div>
+        </div>
+        <div className="dnd-group">
+          <div className="group-title">Group 1</div>
+          <div className="dnd-item">
+            <div>
+              <p>ITEM 1</p>
+            </div>
+          </div>
+          <div className="dnd-item">
+            <div>
+              <p>ITEM 2</p>
+            </div>
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 }
